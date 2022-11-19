37650d99dbbda5773bfc018ccbd47d02c5b67660
thewemonster <wes@thewemonster.com>
2022-11-18 21:58:26 -0800

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,53 @@
+/*
+Author: thewemonster
+Purpose: Learn rust by implementing some basics of the language
+*/
+
 #![allow(unused)]
 
 use clap::Parser;
 use regex::Regex;
 
+const EMAIL_REGEX: &str = r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})";
+
 #[derive(Parser)]
 struct Cli {
-    /// the input from the user; supposed to be an email.
+    // the input from the user; supposed to be an email.
+    // accepts things like username@domain.com, username+qualifier@domain.com,
+    // uname.things@sub.domain.com. uname.things+qual@sub.domain.com
     input: String,
 }
 
+#[derive(Debug)]
 struct Email {
-    // for example [username: myuname, extra_qualifier: +, domain: gmail.com]
     username: String,
     extra_qualifier: String,
     domain: String,
 }
 
 impl Email {
-    pub fn new() -> Self {
-        Self {
-            username: String::new(),
-            extra_qualifier: String::new(),
-            domain: String::new(),
+    pub fn new(input: String) -> Self {
+        if Regex::new(&EMAIL_REGEX).unwrap().is_match(&input) {
+            let items: Vec<&str> = input.split(&['+', '@'][..]).collect();
+            return match items.len() {
+                2 => Self {
+                    username: String::from(items[0]),
+                    extra_qualifier: String::new(),
+                    domain: String::from(items[1]),
+                },
+                3 => Self {
+                    username: String::from(items[0]),
+                    extra_qualifier: String::from(items[1]),
+                    domain: String::from(items[2]),
+                },
+                _ => panic!("invalid email components.")
+            };
         }
+        panic!("invalid email.");
     }
-    fn to_vec(&self) -> Vec<&str> {
-        vec![&self.username, &self.extra_qualifier, &self.domain]
+    fn prettyprint(&self) {
+        println!("{0: <15} | {1: <15} | {2: <15}", &self.username, &self.extra_qualifier, &self.domain);
     }
-}
-
-fn input_separation(mut email: Email, input: String) -> Email {
-    let email_regex = Regex::new(
-        r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})",
-    )
-    .unwrap();
-    if email_regex.is_match(&input) {
-        let v: Vec<&str> = input.split(&['+', '@'][..]).collect();
-        if v.len() == 3 {
-            email.username = String::from(v[0]);
-            email.extra_qualifier = String::from(v[1]);
-            email.domain = String::from(v[2]);
-        } else if v.len() == 2 {
-            email.username = String::from(v[0]);
-            email.domain = String::from(v[1]);
-        } else {
-            panic!("Invalid email");
-        }
-    } else {
-        panic!("Invalid email");
-    }
-    return email;
 }
 
 fn print_table_header() {
@@ -59,14 +57,9 @@ fn print_table_header() {
     );
 }
 
-fn prettyprint_email(email: Email) {
-    let v: Vec<&str> = email.to_vec();
-    println!("{0: <15} | {1: <15} | {2: <15}", v[0], v[1], v[2]);
-}
-
 fn main() {
     let args = Cli::parse();
-    let mut email = input_separation(Email::new(), args.input);
+    let email = Email::new(args.input);
     print_table_header();
-    prettyprint_email(email);
+    email.prettyprint();
 }
